486474e7ed08a9b88a04a2e55f2b161744ac836e
Vaishnavi Srivastava <vaishnavisrivastava.mail@gmail.com>
2026-07-05 09:39:53 +0530

    added comments

--- src/main/java/com/myorg/vibehub/exception/UserNotFoundException.java
+++ src/main/java/com/myorg/vibehub/exception/UserNotFoundException.java
@@ -2,9 +2,12 @@ package com.myorg.vibehub.exception;
 
 public class UserNotFoundException extends RuntimeException {
 
+    //default constructor
     public UserNotFoundException() {
         super("User Not Found!");
     }
+
+    //parameterised constructor
     public UserNotFoundException(String m) {
         super(m);
     }
